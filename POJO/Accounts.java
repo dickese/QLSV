7e804c9cc0243058f8692ccebb791261aba1e690
nhocbinhbo99 <nhocbinhbo99.com>
2025-03-25 18:33:12 +0700

    dao_pojo_acc

---
+++ POJO/Accounts.java
@@ -0,0 +1,61 @@
+package temp;
+
+public class Accounts {
+	private String id;
+	private String username;
+	private String password;
+	private int role;
+	
+	
+	public Accounts(String id, String username, String password, int role) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.role = role;
+	}
+
+
+	public String getId() {
+		return id;
+	}
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+	public String getUsername() {
+		return username;
+	}
+
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public int getRole() {
+		return role;
+	}
+
+
+	public void setRole(int role) {
+		this.role = role;
+	}
+	
+	
+	
+
+}
